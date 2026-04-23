d1d9aa102f70e4642a8e3f340c41abb14d1df1e6
drozd <drozdove912@gmail.com>
2026-04-23 18:06:08 +0300

    inlineButton

--- handlers/router.py
+++ handlers/router.py
@@ -1,8 +1,7 @@
 
 from aiogram import Router, F  
 from aiogram.filters import Command
-from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
-
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup,InlineKeyboardButton,CallbackQuery
 
 router = Router()
 
@@ -17,6 +16,21 @@ def get_main_reply_key():
     )
 
     return keyboard
+def get_main_inline_key():
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text="Открыть сайт", url = "https://chat.deepseek.com/a/chat/s/fc32c22f-f538-491c-977e-ea80267f9dfc")],
+            [InlineKeyboardButton(text="Подробнее",callback_data="info_more")]]
+    )
+
+    return keyboard
+
+
+
+@router.callback_query(lambda c: c.data == "info_more")
+async def process_more_info(callback:CallbackQuery):
+    await callback.message.answer("Вот более подробная информация")
+    await callback.answer()
 
 @router.message(Command("start"))
 @router.message(F.text.lower() == "старт")
@@ -34,4 +48,4 @@ async def help(message:Message):
 @router.message(Command("about"))
 async def about(message:Message):
     print('about')
-    await message.answer(f"Информация про бота. Твое имя: {message.from_user.full_name}")
+    await message.answer(f"Информация про бота. Твое имя: {message.from_user.full_name}",reply_markup=get_main_inline_key())
